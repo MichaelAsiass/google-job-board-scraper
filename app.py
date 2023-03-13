390b98a54978deb14945777ab9bdc3acc8e161b0
MichaelAsiass <46844692+MichaelAsiass@users.noreply.github.com>
2023-03-13 09:09:53 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import requests
+import json
 import matplotlib.pyplot as plt
+from flask import Flask, render_template
+import plotly.graph_objs as go
+import plotly
 from bs4 import BeautifulSoup
 from collections import Counter
 
@@ -10,6 +14,10 @@ headers = {
 
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, 'html.parser')
+
+# app = Flask(__name__)
+
+# @app.route("/")
 
 def get_span(soup, class_name):
     span = str(soup.find_all('span', class_=class_name)).replace('<br/>', '^')
@@ -24,23 +32,22 @@ text = get_span(soup, class_name)
 words = text.split()
 word_count = Counter(words)
 
-search_words = {"Excel", "UX", "Python", "JavaScript", 'SQL', "Java","Angular",
-                "UI", "Rust", "React", "React Native", "MongoDB", "Express", "Node", "PostGreSQL", "Vue", "Tailwind", "SAAS", "Flask", "Django", "ExpressJS", "Swift", "C#", "C", "C++", "HTML", "CSS", ".Net", "XML", "Jenkins", "Kafka", "Redis", "Ruby", "Git", "Microsoft Azure", "Amazon Web Services", "Google Cloud Platform", "Dockers", "Linux", "Kubernetes", "Ubuntu"}
+search_words = {"Excel", "UX", "Python", "JavaScript", 'SQL', "Java", "Angular",
+                "UI", "Oracle", "Jira", "MS SQL Server", "Attention to detail", "Excellent communication skills", "Windows", "NoSQL", "GraphQL", "Rust", "React", "React Native", "MongoDB", "Express", "Node", "PostGreSQL", "Vue", "Tailwind", "SAAS", "Flask", "Django", "ExpressJS", "Swift", "C#", "C", "C++", "HTML", "CSS", ".Net", "Gitlab", "XML", "Jenkins", "Kafka", "Redis", "Ruby", "Git", "Microsoft Azure", "Amazon Web Services", "Google Cloud Platform", "Dockers", "Linux", "Kubernetes", "Ubuntu"}
 
 results = {}
 
 for word in search_words:
     if word in word_count:
         results[word] = word_count[word]
-print(results)
 
 #create a bar chart of the results
 def create_bar_chart():
     plt.bar(range(len(results)), list(results.values()), align='center')
     plt.xticks(range(len(results)), list(results.keys()), rotation=90)
-    plt.xlabel('Search words')
-    plt.ylabel('No. of times this skill is mentioned on Job Postings')
-    plt.title('Skill required on Job Postings')
+    plt.xlabel('Skills / Programming Languages')
+    plt.ylabel('No. of times this skill is mentioned on Job Google Job Board')
+    plt.title('Google Job Board Data')
     plt.show()
 
 create_bar_chart()
